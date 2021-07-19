334b762565c14e82a114b1e0156caa07417d796d
Sarah-MB97 <bailey.sarah@outlook.com>
2021-07-19 14:27:15 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <footer>
-        Coded by Sarah Bailey and is {""}
-        <a
-          href="https://github.com/Sarah-MB97/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+    <div className="container">
+      <div className="App">
+        <footer>
+          Coded by Sarah Bailey and is {""}
+          <a
+            href="https://github.com/Sarah-MB97/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
