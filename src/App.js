2d564792d5f15e69461ef2a549a76b178cb5ea98
Sarah-MB97 <bailey.sarah@outlook.com>
2021-07-19 14:08:22 +0200

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <footer>
+        Coded by Sarah Bailey and is {""}
+        <a
+          href="https://github.com/Sarah-MB97/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
-
- 
